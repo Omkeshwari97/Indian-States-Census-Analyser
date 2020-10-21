2fad0b7ef56cea4cb50c92f879677dd860e79251
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-21 20:26:41 +0530

    Welcome message

---
+++ src/main/java/IndianStatesCensusAnalyser/Analyser.java
@@ -0,0 +1,9 @@
+package IndianStatesCensusAnalyser;
+
+public class Analyser 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Indian States Census Analyser");
+	}
+}
